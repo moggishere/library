184ba324850acd802d7222e759bf27fffd7160dd
moggishere <aryyoon@gmail.com>
2021-12-03 00:47:24 -0300

    basic html/css added

---
+++ script.js
@@ -0,0 +1,18 @@
+let myLibrary = []; 
+
+function Book(title, author, pages, status) {
+    this.title = title; //string
+    this.author = author; //string
+    this.pages = pages; // integer
+    this.status = status; // BOOLEAN?
+    // this.info = function () {
+    //     return (`${title} was written by ${author}, it has 
+    //     ${pages} pages and I have ${status}.`)
+    // }
+}
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
+
+function addBookToTheLibrary() {
+
+}
